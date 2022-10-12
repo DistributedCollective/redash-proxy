b4d4497ff21006baff2a3c70b38fa89ba093fd19
BetsyBraddock <69518422+BetsyBraddock@users.noreply.github.com>
2022-10-12 16:45:39 +0100

    Unit tests (#4)
    * Proxy service
    
    * Cleanup
    
    * Cleanup
    
    * proxy api tests
    
    * Lint
    
    * Lint

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,6 +1,7 @@
 /* eslint-env jest */
 import app from './app'
 import request from 'supertest'
+import axios from 'axios'
 
 it('testing to See if Jest Works', () => {
   expect(1).toBe(1)
@@ -13,5 +14,44 @@ describe('GET /', () => {
     expect(result.text).toBe(
       'Sovryn Redash Proxy Service Running. Stay Sovryn.'
     )
+  })
+})
+
+describe('GET /proxy/*', () => {
+  it('returns correct redash API response for public endpoint', async () => {
+    const redashResult = await axios.get(
+      'https://redash.sovryn.app/api/queries/207/results.json?api_key=LkO9lvuzaAnKXihpH855A90yPlEKvjbitBR4KWLm'
+    )
+    const result = await request(app)
+      .get(
+        '/proxy/api/queries/207/results.json?api_key=LkO9lvuzaAnKXihpH855A90yPlEKvjbitBR4KWLm'
+      )
+      .send()
+    expect(result.status).toBe(200)
+    expect(result.body).toEqual(redashResult.data)
+  })
+
+  it('returns error for wrong API key', async () => {
+    const redashQuery = async (): Promise<any> => {
+      return await axios
+        .get(
+          'https://redash.sovryn.app/api/queries/207/results.json?api_key=LkO9lvuzaAnKXihpH855A90yPlEKvjbitBR4KWLg'
+        )
+        .then((res) => {
+          return res
+        })
+        .catch((e) => {
+          return e.response
+        })
+    }
+    const redashResult = await redashQuery()
+    const result = await request(app)
+      .get(
+        '/proxy/api/queries/207/results.json?api_key=LkO9lvuzaAnKXihpH855A90yPlEKvjbitBR4KWLg'
+      )
+      .send()
+    console.log(redashResult)
+    expect(result.status).toBe(redashResult.status)
+    expect(result.body).toEqual(redashResult.data)
   })
 })
